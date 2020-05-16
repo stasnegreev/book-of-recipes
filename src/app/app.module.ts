96c5244ee81fe3e894de4e25704d2bc383aed9c4
StasNegreev <stasnegreev@gmail.com>
2020-05-16 17:41:10 +0300

    feat(BRCP#1):add shared module; add header component; add aside-menu component;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
@@ -8,10 +7,15 @@ import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { HeaderComponent } from './header/header.component';
+import { SharedModule } from './shared/shared.module';
+import { AsideMenuComponent } from './aside-menu/aside-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideMenuComponent
   ],
   imports: [
     BrowserModule,
@@ -20,6 +24,7 @@ import { MatSliderModule } from '@angular/material/slider';
     AngularFirestoreModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  @Input()
+  isOpenMenuButtonActive = false;
+
+  @Output()
+  public clickMenuButton = new EventEmitter<void>();
+
+  onToggleMenu() {
+    this.clickMenuButton.emit();
+  }
+}
